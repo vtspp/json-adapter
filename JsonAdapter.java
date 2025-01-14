c04b468b61173960d99edb415f866941d2f3c9e1
Victor Pinho <49285474+vtspp@users.noreply.github.com>
2025-01-14 19:27:05 -0300

    Add documentation

--- JsonAdapter.java
+++ JsonAdapter.java
@@ -16,12 +16,21 @@ public final class JsonAdapter {
 
     private Map<Object, Object> readValues;
 
-    private JsonAdapter() {}
+    private JsonAdapter() {
+        throw new RuntimeException("A classe não deve ser instanciada. Utilize o padrão builder para a criação de uma nova instância.");
+    }
 
     private JsonAdapter(final Map<Object, Object> readValues) {
         this.readValues = readValues;
     }
 
+    /**
+     * @param json Parâmetro que será usado para converter em um mapa
+     * @param target Key usada para identificar qual o dado que será adaptado.
+     *               Caso o item não seja identificado, nenhum dado será alterado e o mesmo json será retornado.
+     * @param function Função usada para alterar o dados identificado no target.
+     * @return Retorna um novo json com o dado alterado. Em casos de erro ou caso não seja identificado, o mesmo json de origem será retornado.
+     */
     public static String adapterSingleValue(String json, final String target, final Function<Object, Object> function) {
         try {
             var map = MAPPER.readValue(json, Map.class);
@@ -39,11 +48,24 @@ public final class JsonAdapter {
         }
     }
 
+    /**
+     * @param json Parâmetro em formato de json que será usado para popular os dados do mapa contido na instância
+     * @return Retorna uma instância da classe JsonAdapter. Com ela é possível modificar mais de um dado recebido no arquivo json
+     * @throws IOException
+     */
     public static JsonAdapter createJsonAdapterBuild(String json) throws IOException {
         final var readValues = MAPPER.readValue(json, Map.class);
         return new JsonAdapter(readValues);
     }
 
+    /**
+     * @param target Key utilizada para recuperar o valor dentro do mapa
+     * @param function Parâmetro em forma de função. Pode ser utilizado como argumento a função disponível na classe JsonAdapterCR ou
+     *                 uma outra função personalizada conforme a necessidade.
+     *
+     * @return Retorna a instância de um JsonAdapterCR, tornando flexivél para ser chamado multiplas vezes a fim de modificar outros parâmetros contidos no mapa.
+     * Basta fornecer uma nova function a cada chamada.
+     */
     public JsonAdapter adapterByFunction(final String target, final Function<Object, Object> function) {
         if (this.readValues.containsKey(target)) {
             final var valueByFunction = function.apply(this.readValues.get(target));
@@ -52,12 +74,18 @@ public final class JsonAdapter {
         return this;
     }
 
+    /**
+     * @return Transforma todos os valores armazenados na instância do mapa em um novo json
+     * @throws JsonProcessingException Uma exception será lançada caso algum erro ocorra. Precisa ser tratado por que utilizar.
+     */
     public String buildToJson() throws JsonProcessingException {
         return MAPPER.writeValueAsString(this.readValues);
     }
 
-
-
+    /**
+     * @param maxSize Parâmetro usado para definir a quantidade máxima de caracteres
+     * @return Retorna uma Function que modifica a string conforme a quantidade de caracteres recebida como parâmetro
+     */
     public static Function<Object, Object> functionMaxSizeAccepted(int maxSize) {
         final var initPosition = 0;
         final var endPosition = Math.max(maxSize, 1);
